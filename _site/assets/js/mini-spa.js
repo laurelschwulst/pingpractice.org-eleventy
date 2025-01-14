d360a1b7ace3392a7f7988e09b22a2c7bd0d84c2
Taichi Aritomo <taichiaritomo@gmail.com>
2025-01-14 18:57:38 -0500

    big refactor to make all the fade effects work

--- _site/assets/js/mini-spa.js
+++ _site/assets/js/mini-spa.js
@@ -6,10 +6,10 @@
    * @param {string} href URL to route to
    * @param {boolean} pushState whether to call history.pushState or not
    */
-  function load(href, pushState) {
+  async function load(href, pushState) {
     const container = $("main");
     const xhr = new XMLHttpRequest();
-    xhr.onload = function () {
+    xhr.onload = async function () {
       const d = xhr.responseXML;
       const dTitle = d.title || "";
       const dContainer = $("main", d);
@@ -23,7 +23,8 @@
       updateMainMenu(href);
       updateBodyClass(href);
       updateTransmissionLayout(href);
-      // console.log("laurel!");
+      document.body.classList.remove('loading');
+      resetFigure();
     };
     xhr.onerror = function () {
       document.location.href = href;
@@ -68,66 +69,18 @@
   }
 
   function updateBodyClass(href) {
-    const body = document.body;
-    const normalizedHref = normalizeUrl(href);
-
-    // Extract the last part of the URL path, or set to 'home' for the root
-    const path = normalizedHref.split("/").filter(Boolean).pop() || "home";
-
-    // Check if the URL starts with /transmissions/ and has more than 2 segments
-    const isTransmission =
-      normalizedHref.startsWith("/transmissions/") &&
-      normalizedHref.split("/").length > 2;
-
-    // Set the timeout duration: 3000ms if it's a transmission, otherwise 1500ms
-    const timeoutDuration = isTransmission ? 1500 : 1250;
-
-    // Set the body class
-    setTimeout(function () {
-      body.className = isTransmission ? "transmission" : path; // Use "transmission" if it's a transmission, otherwise the path
-      document
-        .querySelectorAll("img.overlay")
-        .forEach((img) => img.classList.remove("fadeout"));
-    }, timeoutDuration);
+    const isTransmission = getIsTransmission(href)
+    if (isTransmission) {
+      document.body.className = "transmission"
+    } else {
+      // Extract the last part of the URL path, or set to 'home' for the root
+      const normalizedHref = normalizeUrl(href);
+      const path = normalizedHref.split("/").filter(Boolean).pop() || "home";
+      document.body.className = path
+    }
   }
 
-  function updateTransmissionLayout(href) {
-    // Use URL parsing to ensure we have a path like /transmissions/something
-    const urlPath = new URL(href, document.baseURI).pathname; // Parse the pathname from the href
-    const isTransmission =
-      urlPath.startsWith("/transmissions/") && urlPath.split("/").length > 3;
-
-    const figure = document.querySelector("figure.pp");
-    const mainmenu = document.querySelector("nav#primary");
-
-    if (figure) {
-      if (isTransmission) {
-        // Fade out the figure if it's a transmission detail page
-        console.log("it's a transmission page");
-        figure.style.transition = "opacity 0.5s ease-in-out"; // Smooth fade
-        figure.style.opacity = 0; // Fade out
-        mainmenu.style.transition = "opacity 0.5s ease-in-out"; // Smooth fade
-        mainmenu.style.opacity = 0; // Fade out
-
-        // Wait 1500ms and then hide it (make it not take up space)
-        setTimeout(function () {
-          figure.style.display = "none"; // Make it disappear from the layout
-          mainmenu.style.display = "none"; // Make it disappear from the layout
-        }, 1500); // 1500ms delay
-      }
-      else {
-        // Ensure it's visible again in the layout
-        figure.style.display = "block"; // Make sure it takes up space in the layout
-        figure.style.transition = "opacity 0.5s ease-in-out"; // Smooth fade
-        mainmenu.style.display = "block"; // Make sure it takes up space in the layout
-        mainmenu.style.transition = "opacity 0.5s ease-in-out"; // Smooth fade
-        setTimeout(function () {
-          figure.style.opacity = 1; // Fade in
-          mainmenu.style.opacity = 1; // Fade in
-        }, 500)
-      }
-    }
-
+  function updateTransmissionLayout() {
     // transmission backlink at the bottom of a transmission page
     const backToTransmissionButton = document.querySelector('nav#bottom a.transmissions-link')
     if (backToTransmissionButton) {
@@ -175,33 +128,10 @@
         return;
       }
 
-      // if the link is "back to transmissions"
-      // do the opposite of this:
-
-      // const figure = document.querySelector("figure.pp");
-      // const mainmenu = document.querySelector("nav#primary");
-      // figure.style.opacity = 1;
-      // mainmenu.style.opacity = 1;
-      // figure.style.display = "block";
-      // mainmenu.style.display = "none";
-
-      // setTimeout(function () {
-      //   figure.style.opacity = 1;
-      //   mainmenu.style.opacity = 1;
-      // }, 1500);
-
       if (href.startsWith(baseUrl) || el.classList.includes("spa-link")) {
         console.log("handle SPA!!!!!!");
         evt.preventDefault();
-        let modifiedHref = href.replace(/^\/|\/$/g, "");
-        document.querySelector(`div.content`).classList.add("fadeout");
-        document
-          .querySelectorAll("img.overlay")
-          .forEach((img) => img.classList.add("fadeout"));
-        setTimeout(function () {
-          load(href, true);
-        }, 500);
-        updateMainMenu(href);
+        handleSpaNavigation(href)
       }
     }
   });
@@ -210,4 +140,73 @@
     // remove?
     load(document.location.pathname, false);
   });
+
+  async function handleSpaNavigation(newHref) {
+    document.body.classList.add("loading")
+
+    // if we are navigating to transmission page, handle layout
+    const currentHref = document.location.href
+    const isNewHrefTransmission = getIsTransmission(newHref)
+    if (isNewHrefTransmission) {
+      removeFigure()
+      await sleep(500)
+      resetFigure()
+      document.body.classList.add('transmission')
+    }
+
+    // if we are navigating away from transmission page, handle layout
+    const isCurrentHrefTransmission = getIsTransmission(currentHref)
+    const unloadTransmission = isCurrentHrefTransmission && !isNewHrefTransmission
+    if (unloadTransmission) {
+      document.body.classList.add('unload-transmission')
+      setTimeout(() => {
+        document.body.classList.remove('unload-transmission')
+        document.body.classList.remove('transmission')
+        const mainmenu = document.querySelector("nav#primary");
+        const figure = document.querySelector("figure.pp");
+        mainmenu.style.opacity = 0;
+        figure.style.opacity = 0;
+      }, 500)
+    }
+
+    // if we are navigating to home page, handle layout
+    if (newHref === '/') {
+      document.body.classList.add('home')
+    }
+
+    // wait 500ms for fadeout animations, then load
+    setTimeout(function () {
+      load(newHref, true);
+    }, 500);
+    // updateMainMenu(href);
+  }
+
+  function getIsTransmission(href) {
+    const normalizedHref = normalizeUrl(href);
+
+    // Check if the URL starts with /transmissions/ and has more than 2 segments
+    const isTransmission =
+      normalizedHref.startsWith("/transmissions/") &&
+      normalizedHref.split("/").length > 2;
+    
+    return isTransmission
+  }
+
+  function removeFigure() {
+    const mainmenu = document.querySelector("nav#primary");
+    const figure = document.querySelector("figure.pp");
+    figure.style.opacity = 0; // Fade out
+    mainmenu.style.opacity = 0; // Fade out
+  }
+
+  function resetFigure() {
+    const mainmenu = document.querySelector("nav#primary");
+    const figure = document.querySelector("figure.pp");
+    figure.style.opacity = ''
+    mainmenu.style.opacity = ''
+  }
+
+  function sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time))
+  }
 })();
