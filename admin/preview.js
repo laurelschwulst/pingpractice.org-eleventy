b793c2b1febb4a494d0b00bc868a9576f0bb873a
Laurel Schwulst <laurel.schwulst@gmail.com>
2025-01-15 21:00:16 -0500

    up

--- admin/preview.js
+++ admin/preview.js
@@ -1,31 +1,10 @@
-// Confirm that preview.js is loaded
 console.log("Preview.js is loaded!");
 
-// Import CMS library
-import CMS from "netlify-cms-app";
-
-// Custom preview template for "Pages"
+// Custom preview for "Pages"
 const PagePreview = () => `
-  <div style="background-color: yellow; height: 100px; padding: 20px;">
+  <div style="background-color: yellow; height: 100px;">
     <h1>Custom Template Applied!</h1>
-    <p>This is a test preview for Pages collection with a yellow background.</p>
   </div>
 `;
 
-// Custom preview template for "Transmissions"
-const TransmissionPreview = ({ entry, widgetFor }) => {
-  const data = entry.getIn(["data"]).toJS();
-
-  // Return custom HTML for the transmission preview
-  return `
-    <article class="transmission-preview" style="background-color: yellow; padding: 20px;">
-      <h1>${data.title}</h1>
-      <time>${data.date}</time>
-      <div class="content">${widgetFor("body")}</div>
-    </article>
-  `;
-};
-
-// Register the preview templates
 CMS.registerPreviewTemplate("pages", PagePreview);
-CMS.registerPreviewTemplate("transmissions", TransmissionPreview);
