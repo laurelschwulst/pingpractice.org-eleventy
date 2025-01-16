813cdeb2943c0f9f1d241ff61e7ce9a10a09900d
Laurel Schwulst <laurel.schwulst@gmail.com>
2025-01-15 21:34:02 -0500

    up

--- _site/admin/preview.js
+++ _site/admin/preview.js
@@ -1,31 +1,25 @@
-// Confirm that preview.js is loaded
 console.log("Preview.js is loaded!");
 
-// Import CMS library
-import CMS from "netlify-cms-app";
-
-// Custom preview template for "Pages"
-const PagePreview = () => `
-  <div style="background-color: yellow; height: 100px; padding: 20px;">
-    <h1>Custom Template Applied!</h1>
-    <p>This is a test preview for Pages collection with a yellow background.</p>
-  </div>
-`;
-
-// Custom preview template for "Transmissions"
-const TransmissionPreview = ({ entry, widgetFor }) => {
-  const data = entry.getIn(["data"]).toJS();
-
-  // Return custom HTML for the transmission preview
+const PagePreview = () => {
+  console.log("PagePreview executed!"); // Debug log
   return `
-    <article class="transmission-preview" style="background-color: yellow; padding: 20px;">
-      <h1>${data.title}</h1>
-      <time>${data.date}</time>
-      <div class="content">${widgetFor("body")}</div>
-    </article>
-  `;
+      <div style="background-color: yellow; height: 100px; padding: 20px;">
+        <h1>Custom Template Applied!</h1>
+      </div>
+    `;
 };
 
-// Register the preview templates
+const TransmissionPreview = ({ entry, widgetFor }) => {
+  console.log("TransmissionPreview executed!"); // Debug log
+  const data = entry.getIn(["data"]).toJS();
+  return `
+      <article style="background-color: yellow; padding: 20px;">
+        <h1>${data.title}</h1>
+        <time>${data.date}</time>
+        <div>${widgetFor("body")}</div>
+      </article>
+    `;
+};
+
 CMS.registerPreviewTemplate("pages", PagePreview);
 CMS.registerPreviewTemplate("transmissions", TransmissionPreview);
--- admin/preview.js
+++ admin/preview.js
@@ -1,10 +1,25 @@
 console.log("Preview.js is loaded!");
 
-// Custom preview for "Pages"
-const PagePreview = () => `
-  <div style="background-color: yellow; height: 100px;">
-    <h1>Custom Template Applied!</h1>
-  </div>
-`;
+const PagePreview = () => {
+  console.log("PagePreview executed!"); // Debug log
+  return `
+      <div style="background-color: yellow; height: 100px; padding: 20px;">
+        <h1>Custom Template Applied!</h1>
+      </div>
+    `;
+};
+
+const TransmissionPreview = ({ entry, widgetFor }) => {
+  console.log("TransmissionPreview executed!"); // Debug log
+  const data = entry.getIn(["data"]).toJS();
+  return `
+      <article style="background-color: yellow; padding: 20px;">
+        <h1>${data.title}</h1>
+        <time>${data.date}</time>
+        <div>${widgetFor("body")}</div>
+      </article>
+    `;
+};
 
 CMS.registerPreviewTemplate("pages", PagePreview);
+CMS.registerPreviewTemplate("transmissions", TransmissionPreview);
