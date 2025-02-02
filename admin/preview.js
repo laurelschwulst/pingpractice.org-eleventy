aea5c643f42a3331ef7fa842c6f451722acdaa48
Laurel Schwulst <laurel.schwulst@gmail.com>
2025-02-02 14:02:28 -0500

    test crimson

--- admin/preview.js
+++ admin/preview.js
@@ -2,9 +2,24 @@ console.log("Preview.js is loaded!");
 
 const StaticPreview = () => {
   console.log("StaticPreview executed!");
+  const title = entry.getIn(["data", "title"]) || "No title";
+  const content = entry.getIn(["data", "body"]) || "No content";
+
+  return React.createElement(
+    "div",
+    { style: { backgroundColor: "yellow", padding: "20px" } },
+    React.createElement("h1", null, title),
+    React.createElement("div", null, content)
+  );
 };
 
 console.log("Registering StaticPreview...");
-CMS.registerPreviewTemplate("about", StaticPreview);
+
+// CMS.registerPreviewTemplate("about", StaticPreview);
+
+// Register each page
+["about", "method", "app", "people"].forEach((page) => {
+  CMS.registerPreviewTemplate(page, StaticPreview);
+});
 
 CMS.registerPreviewStyle("/admin/custom-preview.css");
