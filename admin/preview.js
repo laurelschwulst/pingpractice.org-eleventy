0f999aeefe88fb5801237efc9142f6bf43af6d01
Laurel Schwulst <laurel.schwulst@gmail.com>
2025-02-02 12:59:15 -0500

    test

--- admin/preview.js
+++ admin/preview.js
@@ -1,17 +1,10 @@
 console.log("Preview.js is loaded!");
 
 const StaticPreview = () => {
-  console.log("StaticPreview executed!"); // Debug log
-  return React.createElement(
-    "div",
-    { style: { backgroundColor: "yellow", padding: "20px" } },
-    React.createElement("h1", null, "Static Title"),
-    React.createElement("p", null, "This is some static content for testing.")
-  );
+  console.log("StaticPreview executed!");
 };
 
 console.log("Registering StaticPreview...");
-CMS.registerPreviewTemplate("pages", StaticPreview);
+CMS.registerPreviewTemplate("about", StaticPreview);
 
-// Add custom preview styles (optional, can test without it)
 CMS.registerPreviewStyle("/admin/custom-preview.css");
